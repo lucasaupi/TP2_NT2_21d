d95fd10ffa9e391bf3ba712f7fdd045da62f30af
Lucas <lucas-aupi@hotmail.com>
2024-09-10 20:29:17 -0300

    tp1 Lucas Aupi

---
+++ TP1/index.js
@@ -0,0 +1,24 @@
+
+const fs = require('fs');
+const condicion = 'number';
+
+function crearNuevoArray(array, condicion) {
+    if (!['string', 'number', 'boolean'].includes(condicion)) {
+        return "Error condición no válida";
+    }
+
+    const nuevoArray = array.filter(elemento => typeof elemento === condicion);
+
+    const arrayOrdenado = [...new Set(nuevoArray)].sort((a, b) => a > b ? 1 : -1);
+
+    if (arrayOrdenado.length > 0) {
+        const datos = arrayOrdenado.join(', ');
+        fs.writeFileSync('resultado.txt', datos);
+        console.log('Nuevo array guardado');
+    } else {
+        console.log('Array vacío');
+    }
+}
+
+const array = [2, 10, "a", 4, "b", 6, "d", true, "e", 9, 1, "z", 12, "r", "c", false];
+crearNuevoArray(array, condicion);
---
+++ TP1/readme.md
@@ -0,0 +1,2 @@
+Correr en la terminal el codigo 'node .\index.js' para ejecutar la funcion con el array preseteado en la constante.
+El resultado del array filtrado esta en el archivo resultado.txt.
